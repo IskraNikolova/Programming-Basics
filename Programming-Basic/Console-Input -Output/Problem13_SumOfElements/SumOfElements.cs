3f05ac913749ef03412bf9e171f0f1203e8fb3c5
Iskra Nikolova <air_iskra@abv.bg>
2015-07-25 16:28:34 +0300

    Add problem13

---
+++ Programming-Basic/Console-Input -Output/Problem13_SumOfElements/SumOfElements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+public class SumOfElements
+{
+    public static void Main()
+    {
+        long[] inputNumbers = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+        long avarageSum = inputNumbers.Max();
+        long sum = 0;
+        bool isFirstMaxNumber = true;
+        for (int i = 0; i < inputNumbers.Length; i++)
+        {
+            if (inputNumbers[i] == avarageSum && isFirstMaxNumber)
+            {
+                inputNumbers[i] = 0;
+                isFirstMaxNumber = false;
+            }
+
+            sum += inputNumbers[i];
+        }
+
+        if (avarageSum == sum)
+        {
+            Console.WriteLine("Yes, sum={0}", avarageSum);
+        }
+
+        else
+        {
+            long diff = Math.Abs(sum - avarageSum);
+            Console.WriteLine("No, diff={0}", diff);
+        }
+    }
+}
+
