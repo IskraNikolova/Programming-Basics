f98637d1616cedb837f1a9f334a8495df0b5dab4
Iskra Nikolova <air_iskra@abv.bg>
2015-07-25 16:29:17 +0300

    Add problem14

---
+++ Programming-Basic/Console-Input -Output/Problem14_NewHouse/NewHouse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class NewHouse
+{
+    public static void Main()
+    {
+        int inputNumber = int.Parse(Console.ReadLine());
+
+        string elementOfFirstLine = new string('-',(inputNumber - 1)/2);
+        string firstAsterisk = "*";
+        Console.WriteLine("{0}{1}{2}", elementOfFirstLine, firstAsterisk, elementOfFirstLine);
+
+        int countOfAsterisk = 3;
+        int lengthForLoop = inputNumber/2;
+        for (int i = 0; i < lengthForLoop; i++)
+        {
+            int countOfDash = (inputNumber - countOfAsterisk) / 2;
+            string dash = new string('-', countOfDash);
+            string asterisks = new string('*', countOfAsterisk);
+            Console.WriteLine("{0}{1}{0}", dash, asterisks);
+            countOfAsterisk += 2;
+        }
+
+        for (int i = 0; i < inputNumber; i++)
+        {
+            string downAsterisk = new string('*', inputNumber - 2);
+            Console.WriteLine("|{0}|", downAsterisk);
+        }
+    }
+}
+
