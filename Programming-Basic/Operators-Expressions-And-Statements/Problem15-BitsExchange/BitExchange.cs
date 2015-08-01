e2c6297595145b0ef998f44f8726d6c46d9ab9fe
Iskra Nikolova <air_iskra@abv.bg>
2015-08-01 21:14:29 +0300

    Add a problem

---
+++ Programming-Basic/Operators-Expressions-And-Statements/Problem15-BitsExchange/BitExchange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class BitExchange
+{
+    /// <summary>
+    /// Write a program that exchanges bits 3, 4 and 5 with bits 24, 25 and 26 of given 32-bit unsigned integer
+    /// </summary>
+    public static void Main()
+    {
+        const int firstPosition = 3;
+        const int secondPosition = 24;
+
+        uint inputNumber = uint.Parse(Console.ReadLine());
+
+        uint firstBits = inputNumber >> firstPosition;
+        firstBits = firstBits & 7;
+
+        uint secondBits = inputNumber >> secondPosition;
+        secondBits = secondBits & 7;
+
+        uint maskFirstBits = 7 << firstPosition;
+        uint maskSecondBits = 7 << secondPosition;
+
+        inputNumber = inputNumber & ~maskFirstBits | (secondBits << firstPosition);
+        inputNumber = inputNumber & ~maskSecondBits | (firstBits << secondPosition);
+
+        Console.WriteLine(inputNumber);
+    }
+}
+
