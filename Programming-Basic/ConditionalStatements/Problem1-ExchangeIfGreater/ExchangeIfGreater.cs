82433dace5ed69faa6ab4ac2d2aeabe431422e0a
Iskra Nikolova <air_iskra@abv.bg>
2015-08-02 15:19:09 +0300

    Add new project

---
+++ Programming-Basic/ConditionalStatements/Problem1-ExchangeIfGreater/ExchangeIfGreater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class ExchangeIfGreater
+{
+    /// <summary>
+    /// Write an if-statement that takes two integer variables a and b and exchanges their values 
+    /// if the first one is greater than the second one. As a result print the values a and b, 
+    /// separated by a space.
+    /// </summary>
+    public static void Main()
+    {
+        double firstInteger = double.Parse(Console.ReadLine());
+        double secondInteger = double.Parse(Console.ReadLine());
+
+        if (firstInteger > secondInteger)
+        {
+            Console.WriteLine(;
+        }
+    }
+}
+
