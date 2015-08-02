a0ad51ef5e5b16bf5ef556e60b8c005c07f634a7
Iskra Nikolova <air_iskra@abv.bg>
2015-08-02 12:54:18 +0300

    Add new problem

---
+++ Programming-Basic/Operators-Expressions-And-Statements/Problem20-BitsUp/BitsUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class BitsUp
+{
+    public static void Main()
+    {
+        const int count = 7;
+
+        int countOfNumbers = int.Parse(Console.ReadLine());
+        int step = int.Parse(Console.ReadLine());
+        int index = 0;
+
+        for (int i = 0; i < countOfNumbers; i++)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            for (int j = count; j >= 0; j--)
+            {
+                if ((index%step == 1) || (step == 1 && index > 0))
+                {
+                    int mask = 1 << j;
+                    inputNumber = inputNumber | mask;
+                }
+                index++;
+            }
+            Console.WriteLine(inputNumber);
+        }
+    }
+}
+
