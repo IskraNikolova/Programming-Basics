aaace467d0ca84d4e34340d4d9b631c621fd58ef
Iskra Nikolova <air_iskra@abv.bg>
2015-08-02 23:22:25 +0300

    changes

--- Programming-Basic/ConditionalStatements/Problem3-CheckForAPlayCard/CheckForAPlayCard.cs
+++ Programming-Basic/ConditionalStatements/Problem3-CheckForAPlayCard/CheckForAPlayCard.cs
@@ -3,6 +3,11 @@ using System.Linq;
 
 public class CheckForAPlayCard
 {
+    /// <summary>
+    /// Classical play cards use the following signs to designate the card
+    ///  face: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K and A. Write a program that enters a string 
+    /// and prints “yes” if it is a valid card sign or “no” otherwise.
+    /// </summary>
     public static void Main()
     {
         string[] playCard = new[] {"2", "3", "4", "5", "6", "7", "8", "9", "10","J", "Q", "K", "A"};
