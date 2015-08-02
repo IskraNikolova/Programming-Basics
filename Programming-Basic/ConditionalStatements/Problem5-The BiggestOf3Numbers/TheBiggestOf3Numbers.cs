bad051f4e8129a22b36be7610cb171338522e4d0
Iskra Nikolova <air_iskra@abv.bg>
2015-08-02 23:24:26 +0300

    add new Problem

---
+++ Programming-Basic/ConditionalStatements/Problem5-The BiggestOf3Numbers/TheBiggestOf3Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class TheBiggestOf3Numbers
+{
+    /// <summary>
+    /// Write a program that finds the biggest of three numbers
+    /// </summary>
+    public static void Main()
+    {
+        const int countOfNumbers = 3;
+
+        double maxValue = 0;
+        for (int i = 0; i < countOfNumbers; i++)
+        {
+            double inputNumber = double.Parse(Console.ReadLine());
+            if (inputNumber > maxValue)
+            {
+                maxValue = inputNumber;
+            }
+        }
+        Console.WriteLine(maxValue);
+    }
+}
+
