88a6d2ad1709d5e0f7e0b76e5a31b806b97846a4
Iskra Nikolova <air_iskra@abv.bg>
2015-08-02 23:34:58 +0300

    Add new problem

---
+++ Programming-Basic/ConditionalStatements/Problem6-TheBiggestOfFiveNumbers/TheBiggestOfFiveNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class TheBiggestOfFiveNumbers
+{
+    /// <summary>
+    /// Write a program that finds the biggest of five numbers by using only five if statements. 
+    /// </summary>
+    public static void Main()
+    {
+        const int countOfNumbers = 5;
+
+        double maxValue = 0;
+        for (int i = 0; i < countOfNumbers; i++)
+        {
+            double inputNumber = double.Parse(Console.ReadLine());
+            if (inputNumber > maxValue)
+            {
+                maxValue = inputNumber;
+            }
+        }
+        Console.WriteLine(maxValue);
+    }
+}
