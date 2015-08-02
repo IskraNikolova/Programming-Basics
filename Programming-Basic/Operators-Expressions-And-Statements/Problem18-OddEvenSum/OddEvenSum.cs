04b2ea8d3d574a20f908b1e925e8b973df7121f1
Iskra Nikolova <air_iskra@abv.bg>
2015-08-02 11:22:12 +0300

    Add new problem

---
+++ Programming-Basic/Operators-Expressions-And-Statements/Problem18-OddEvenSum/OddEvenSum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class OddEvenSum
+{
+    public static void Main()
+    {
+        int countOfNumbersCollection = int.Parse(Console.ReadLine());
+
+        int sumOfOddCollection = 0;
+        int sumOfEvenCollection = 0;
+
+        int lengthOfLoop = countOfNumbersCollection*2;
+        for (int i = 1; i <= lengthOfLoop; i++)
+        {
+            int numbersOfCollection = int.Parse(Console.ReadLine());
+            if (i%2 != 0)
+            {
+                sumOfOddCollection += numbersOfCollection;
+            }
+            else
+            {
+                sumOfEvenCollection += numbersOfCollection;
+            }
+        }
+
+        if (sumOfOddCollection == sumOfEvenCollection)
+        {
+            Console.WriteLine("Yes, sum={0}", sumOfOddCollection);
+        }
+        else
+        {
+            int diff = Math.Abs(sumOfOddCollection - sumOfEvenCollection);
+            Console.WriteLine("No, diff={0}", diff);
+        }
+    }
+}
+
