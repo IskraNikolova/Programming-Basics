390af979f6c880c62955b6eb4662f4f72ad27770
Iskra Nikolova <air_iskra@abv.bg>
2015-08-02 10:34:27 +0300

    Add new problem

---
+++ Programming-Basic/Operators-Expressions-And-Statements/Problem17-Volleyball/Volleyball.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Volleyball
+{
+    public static void Main()
+    {
+        const int weekendsInTheYear = 48;
+
+        string leapOrNo = Console.ReadLine();
+        double daysOfHoliday = int.Parse(Console.ReadLine());
+        double daysInHometown = int.Parse(Console.ReadLine());
+
+        //Not at work in 3/4 of the normal weekends.
+        double normalWeekends = ((weekendsInTheYear - daysInHometown) * 3)/4;
+
+        //Plays in 2/3 of the holidays.
+        double playInHolidays = (daysOfHoliday*2)/3;
+
+        double totalPlays = daysInHometown + normalWeekends + playInHolidays;
+
+        if (leapOrNo == "leap")
+        {
+            double addend = totalPlays*0.15;
+            totalPlays = totalPlays + addend;
+        }
+
+        Console.WriteLine((int)totalPlays);
+    }
+}
+
