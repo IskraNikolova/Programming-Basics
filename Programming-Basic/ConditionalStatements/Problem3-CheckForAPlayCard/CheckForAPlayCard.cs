2ce6624292cf0cb79d9b380b5d3a951980b191a7
Iskra Nikolova <air_iskra@abv.bg>
2015-08-02 15:42:07 +0300

    Add new problem

---
+++ Programming-Basic/ConditionalStatements/Problem3-CheckForAPlayCard/CheckForAPlayCard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public class CheckForAPlayCard
+{
+    public static void Main()
+    {
+        string[] playCard = new[] {"2", "3", "4", "5", "6", "7", "8", "9", "10","J", "Q", "K", "A"};
+
+        string checkSymbol = Console.ReadLine();
+
+        if (playCard.Contains(checkSymbol))
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
+
