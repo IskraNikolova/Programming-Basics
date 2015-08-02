4b6405b2f4e0a1f591dbb870dd1770f8ada17f00
Iskra Nikolova <air_iskra@abv.bg>
2015-08-02 23:23:22 +0300

    add new problem

---
+++ Programming-Basic/ConditionalStatements/Problem4-MultiplicationSign/MultiplicationSign.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class MultiplicationSign
+{
+    /// <summary>
+    /// Write a program that shows the sign (+, - or 0) of the product of three real numbers, 
+    /// without calculating it. Use a sequence of if operators
+    /// </summary>
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        int multiplication = a*b*c;
+
+        if (multiplication > 0)
+        {
+            Console.WriteLine("+");
+        }
+        else if (multiplication == 0)
+        {
+            Console.WriteLine("0");
+        }
+        else
+        {
+            Console.WriteLine("-");
+        }
+    }
+}
+
