ec3e33414922d8a8a61d223cafebdc0f2d8073ac
Iskra Nikolova <air_iskra@abv.bg>
2015-08-02 15:41:29 +0300

    Add new problem

---
+++ Programming-Basic/ConditionalStatements/Problem2-BonusScore/BonusScore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class BonusScore
+{
+    /// <summary>
+    /// Write a program that applies bonus score to given score in the range [1…9] by the following 
+    /// </summary>
+    public static void Main()
+    {
+        int score = int.Parse(Console.ReadLine());
+
+        if (score > 0 && score < 4)
+        {
+            int bonusScore = score * 10;
+            Console.WriteLine(bonusScore);
+        }
+        else if (score > 3 && score < 7)
+        {
+            int bonusScore = score * 100;
+            Console.WriteLine(bonusScore);
+        }
+        else if (score > 6 && score < 10)
+        {
+            int bonusScore = score * 1000;
+            Console.WriteLine(bonusScore);
+        }
+        else if (score <= 0 || score > 9)
+        {
+            Console.WriteLine("invalid score");
+        }
+    }
+}
+
