0b5c56156e59fac57a8608e1eea48d553ece9870
Iskra Nikolova <air_iskra@abv.bg>
2015-08-02 12:10:16 +0300

    Add new problem

---
+++ Programming-Basic/Operators-Expressions-And-Statements/Problem19-TheExplorer/Explorer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Explorer
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int count = n/2;
+        string dashCollection = new string('-', count);
+        string asterisk = "*";
+        string firstAndLastLine = String.Format("{0}{1}{0}", dashCollection, asterisk);
+
+        Console.WriteLine(firstAndLastLine);
+
+        for (int i = 0; i < count; i++)
+        {
+            int countOfOutDash = ((n - 3)/2) - i;
+            int countOfInDash = (n - 2) - (countOfOutDash*2);
+
+            string dashCollectionOut = new string('-', countOfOutDash);
+            string dashCollectionIn = new string('-', countOfInDash);
+            string topMiddleLines = String.Format("{0}{1}{2}{1}{0}", dashCollectionOut, 
+                                                                    asterisk, 
+                                                                    dashCollectionIn);
+
+            Console.WriteLine(topMiddleLines);
+         }
+
+        for (int i = 1; i < count; i++)
+        {
+            int countOfDownInDash = (n - 2) - (i*2);
+            string dashCollectionOut = new string('-', i);
+            string dashCollectionIn = new string('-', countOfDownInDash);
+            string downMiddleLines = String.Format("{0}{1}{2}{1}{0}", dashCollectionOut,
+                                                                  asterisk,
+                                                                  dashCollectionIn);
+
+            Console.WriteLine(downMiddleLines);
+         } 
+
+        Console.WriteLine(firstAndLastLine);
+    }
+}
+
