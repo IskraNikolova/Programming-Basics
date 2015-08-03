56a4cc4583a16702bcd32a1eb608f2bb152ee471
Iskra Nikolova <air_iskra@abv.bg>
2015-08-03 15:01:18 +0300

    Add new problem

---
+++ Programming-Basic/ConditionalStatements/Problem9-PlayWithIntDoubleAndString/PlayWithIntDoubleAndString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class PlayWithIntDoubleAndString
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please choose a type:\n1 --> int\n2 --> double\n3 --> string");
+        string inputSelection = Console.ReadLine();
+
+        if (inputSelection == "1")
+        {
+            Console.Write("Please enter a int: ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            int result = inputNumber + 1;
+
+            Console.WriteLine(result);
+        }
+        else if (inputSelection == "2")
+        {
+            Console.Write("Please enter a double: ");
+            double inputNumber = double.Parse(Console.ReadLine());
+            double result = inputNumber + 1;
+
+            Console.WriteLine(result);
+        }
+        else if (inputSelection == "3")
+        {
+            Console.Write("Please enter a string: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input + '*');
+        }
+     }
+}
+
