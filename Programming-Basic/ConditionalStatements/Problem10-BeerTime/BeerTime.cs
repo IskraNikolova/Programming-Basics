2b664c1f7aca30d8ad2c1ca68c1e34450976ff9c
Iskra Nikolova <air_iskra@abv.bg>
2015-08-03 15:01:58 +0300

    Add new problem

---
+++ Programming-Basic/ConditionalStatements/Problem10-BeerTime/BeerTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class BeerTime
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter time");
+        DateTime hourInput = DateTime.Parse(Console.ReadLine());
+        Console.WriteLine(hourInput.ToString("hh:mm tt"));
+
+        DateTime startBeerTime = DateTime.Parse("1:00 PM");
+        DateTime endBeerTime = DateTime.Parse("3:00 AM");
+
+
+        if (hourInput >= startBeerTime || hourInput <= endBeerTime)
+        {
+            Console.WriteLine("beer time");
+        }
+        else
+        {
+            Console.WriteLine("non-beer time");
+        }
+            
+    }
+}
+
