79ec509c4a6bee8a5a02e0be29c2b621c7ce2047
Iskra Nikolova <air_iskra@abv.bg>
2015-08-05 14:31:30 +0300

    Add new homework

---
+++ Programming-Basic/Loops/Problem1-NumbersFrom1ToN/NumbersFrom1ToN.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class NumbersFrom1ToN
+{
+    /// <summary>
+    /// Program that enters from the console a positive integer n and prints all the numbers 
+    /// from 1 to n, on a single line, separated by a space.
+    /// </summary>
+    public static void Main()
+    {
+        int lengthForLoop = int.Parse(Console.ReadLine());
+
+        for (int number = 1; number <= lengthForLoop; number++)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
+
---
+++ Programming-Basic/Loops/Problem2-NumbersNotDivisibleBy3And7/NumbersNotDivisibleBy3And7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class NumbersNotDivisibleBy3And7
+{
+    public static void Main()
+    {
+        int lengthForLoop = int.Parse(Console.ReadLine());
+
+        for (int number = 0; number < lengthForLoop; number++)
+        {
+            if (number != 3 && number != 7)
+            {
+                Console.Write("{0} ", number);
+            }
+        }
+    }
+}
+
