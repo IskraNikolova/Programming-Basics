dd06ac9c5aff9a6955554688b3cd3d97feeab724
Iskra Nikolova <air_iskra@abv.bg>
2015-08-06 16:24:13 +0300

    Add new problem

--- Programming-Basic/Loops/Problem5-CalculateTheSum/CalculateTheSumWithFactorialFormula.cs
+++ Programming-Basic/Loops/Problem5-CalculateTheSum/CalculateTheSumWithFactorialFormula.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 
-public class Calculate1X
+public class CalculateTheSumWithFactorialFormula
 {
+    /// <summary>
+    /// Program that, for a given two integer numbers n and x, 
+    /// calculates the sum S = 1 + 1!/x + 2!/x2 + … + n!/xn. Use only one loop.
+    ///  Print the result with 5 digits after the decimal point.
+    /// </summary>
     public static void Main()
     {
         Console.Write("n = ");
---
+++ Programming-Basic/Loops/Problem7-CalculateCombinations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml.Schema;
+
+public class CalculateCombination
+{
+    /// <summary>
+    /// n! / (k! * (n-k)!) 
+    /// </summary>
+    public static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("k = ");
+        int k = int.Parse(Console.ReadLine());
+
+        int length = k;
+        int result = n;
+        for (int i = 1; i < length; i++)
+        {
+            result *= n - i;
+        }
+
+        int resultK = 1;
+        for (int i = k; i > 1; i--)
+        {
+            resultK *= i;
+        }
+        Console.WriteLine(result/resultK);
+    }
+}
+
