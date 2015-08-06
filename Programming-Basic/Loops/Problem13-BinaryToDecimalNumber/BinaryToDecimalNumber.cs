79660c2216775d74b6bcc19005b8a1c1ed9a1d28
Iskra Nikolova <air_iskra@abv.bg>
2015-08-06 22:50:07 +0300

    Add new problem

---
+++ Programming-Basic/Loops/Problem13-BinaryToDecimalNumber/BinaryToDecimalNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class BinaryToDecimalNumber
+{
+    /// <summary>
+    /// Using loops write a program that converts a binary integer number to its decimal form. 
+    /// The input is entered as string.
+    /// </summary>
+    public static void Main()
+    {
+        Console.WriteLine("Enter a Binary number:");
+        string binaryNumber = Console.ReadLine();
+        byte[] binarics = new byte[binaryNumber.Length];
+        for (int i = 0; i < binaryNumber.Length; i++)
+        {
+            binarics[i] = byte.Parse(Convert.ToString((binaryNumber[i])));
+        }
+        Array.Reverse(binarics);
+
+        long numDecimal = 0;
+        long multiplier = 1;
+        foreach (var digit in binarics)
+        {
+            numDecimal += (digit * multiplier);
+            multiplier *= 2;
+        }
+
+        Console.WriteLine("{0}", numDecimal);
+    }
+}
+
