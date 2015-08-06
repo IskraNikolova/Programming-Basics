7f38cd960a1a50a0b82b149254628fd99ea5fed2
Iskra Nikolova <air_iskra@abv.bg>
2015-08-06 16:48:02 +0300

    add a changes

--- Programming-Basic/Loops/Problem7-CalculateCombinations/Program.cs
+++ Programming-Basic/Loops/Problem7-CalculateCombinations/Program.cs
@@ -4,7 +4,8 @@ using System.Xml.Schema;
 public class CalculateCombination
 {
     /// <summary>
-    ///  A program that calculates n! / (k! * (n-k)!) for given n and k. 
+    ///  A program that calculates n! / (k! * (n-k)!) for given n and k.
+    ///  n! / (k! * (n-k)!) = (n.(n – 1).(n – 2)...(n — k + 1))/(k(k – 1)...3.2.1)
     /// </summary>
     public static void Main()
     {
@@ -14,19 +15,20 @@ public class CalculateCombination
         Console.Write("k = ");
         int k = int.Parse(Console.ReadLine());
 
-        int length = k;
-        int result = n;
-        for (int i = 1; i < length; i++)
+        //(n.(n – 1).(n – 2)...(n — k + 1))
+        int resultN = n;
+        for (int i = 1; i < k; i++)
         {
-            result *= n - i;
+            resultN *= n - i;
         }
 
+        //(k(k – 1)...3.2.1)
         int resultK = 1;
         for (int i = k; i > 1; i--)
         {
             resultK *= i;
         }
-        Console.WriteLine(result/resultK);
+        Console.WriteLine(resultN/resultK);
     }
 }
 
