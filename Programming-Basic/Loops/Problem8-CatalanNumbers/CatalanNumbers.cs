35a4a4d57956e3d5fbe0bdef16c63307a3d6b526
Iskra Nikolova <air_iskra@abv.bg>
2015-08-06 19:19:42 +0300

    add

---
+++ Programming-Basic/Loops/Problem8-CatalanNumbers/CatalanNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class CatalanNumbers
+{
+    /// <summary>
+    /// Write a program to calculate the nth Catalan number by given n
+    /// </summary>
+    public static void Main()
+    {
+        Console.Write("n = ");
+        int lengthOfLoop = int.Parse(Console.ReadLine());
+
+        ulong catalanNumber = Factorial(2*lengthOfLoop)/(Factorial(lengthOfLoop + 1)*Factorial(lengthOfLoop));
+        Console.WriteLine("Catalan(n) {0}", catalanNumber);
+
+    }
+
+    static ulong Factorial(int lengthOfLoop)
+    {
+        ulong factorial = 1;
+        for (uint i = 1; i <= lengthOfLoop; i++)
+        {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+}
+
