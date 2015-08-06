1b85857ba6127e1930fd357a4401b714c079769c
Iskra Nikolova <air_iskra@abv.bg>
2015-08-06 20:49:50 +0300

    Add a new Problem

---
+++ Programming-Basic/Loops/Problem9-MatrixOfNumbers/MatrixOfNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class MatrixOfNumber
+{
+    /// <summary>
+    /// A program that reads from the console a positive integer number n (1 ≤ n ≤ 20) 
+    /// and prints a matrix like in the examples below. Use two nested loops.
+    /// </summary>
+    public static void Main()
+    {
+        int matrixSize = int.Parse(Console.ReadLine());
+        int endIndex = matrixSize;
+
+        for (int row = 1; row <= matrixSize; row++)
+        {
+            for (int column = row; column <= endIndex; column++)
+            {
+                Console.Write("{0} ", column);
+            }
+
+            endIndex++;
+            Console.WriteLine();
+        }
+    }
+}
+
