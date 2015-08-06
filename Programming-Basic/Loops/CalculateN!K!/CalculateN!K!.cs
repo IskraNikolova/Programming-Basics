f5fee44704cff9d5cad195bc2f9230cb45670b18
Iskra Nikolova <air_iskra@abv.bg>
2015-08-06 16:23:28 +0300

    Add new Problem6-CalculateN!K!.csproj

---
+++ Programming-Basic/Loops/CalculateN!K!/CalculateN!K!.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class CalculateNK
+{
+    /// <summary>
+    /// A program that calculates n! / k! for given n and k.
+    /// </summary>
+    public static void Main()
+    {
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("k = ");
+        int k = int.Parse(Console.ReadLine());
+
+        double factorialN = 1;
+        double factorialK = 1;
+
+        for (int i = n; i > 1; i--)
+        {
+            factorialN *= i;
+            if (i <= k)
+            {
+                factorialK *= i;
+            }
+        }
+
+        double result = factorialN/factorialK;
+        Console.WriteLine((int)result);
+    }
+}
+
