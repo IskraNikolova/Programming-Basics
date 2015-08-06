d245776149fdbd53d04ee728c66dbc9b5d7dbc8a
Iskra Nikolova <air_iskra@abv.bg>
2015-08-06 22:31:53 +0300

    Add new problem

---
+++ Programming-Basic/Loops/Problem12-RandomizeTheNumbers 1…N/RandomizeTheNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+public class RandomizeTheNumbers
+{
+    /// <summary>
+    /// A program that enters in integer n and prints the numbers 1, 2, …, n in random order. 
+    /// </summary>
+    public static void Main()
+    {
+        int lengthOfCollection = int.Parse(Console.ReadLine());
+
+        int[] collectionOfNumbers = new int[lengthOfCollection];
+        for (int index = 0; index < lengthOfCollection; index++)
+        {
+            collectionOfNumbers[index] = index;
+        }
+
+        Random random = new Random();
+        collectionOfNumbers = Enumerable.Range(1, lengthOfCollection).OrderBy(r => random.Next()).ToArray();
+
+        for (int index = 0; index < lengthOfCollection; index++)
+        {
+            Console.Write("{0} ", collectionOfNumbers[index]);
+        }
+    }
+}
+
