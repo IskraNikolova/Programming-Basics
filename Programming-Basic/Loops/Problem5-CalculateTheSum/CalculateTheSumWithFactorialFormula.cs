08bc2ae9fee7a991324d520fb847e3bf79ea8b49
Iskra Nikolova <air_iskra@abv.bg>
2015-08-06 14:52:49 +0300

    Add new problem

---
+++ Programming-Basic/Loops/Problem5-CalculateTheSum/CalculateTheSumWithFactorialFormula.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Calculate1X
+{
+    public static void Main()
+    {
+        Console.Write("n = ");
+        int lengthOfLoop = int.Parse(Console.ReadLine());
+
+        Console.Write("x = ");
+        int x = int.Parse(Console.ReadLine());
+
+        double factorial = 1;
+        double divisor = 1;
+        double sum = 1;
+        for (int i = 1; i <= lengthOfLoop; i++)
+        {
+            factorial *= i;
+            divisor *= x;
+            sum += factorial/divisor;
+        }
+
+        Console.WriteLine("{0:F5}", sum);
+    }
+}
+
