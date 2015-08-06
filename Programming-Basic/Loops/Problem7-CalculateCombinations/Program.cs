813c0d4d48be7381b355957b880bba7e4c56cd9a
Iskra Nikolova <air_iskra@abv.bg>
2015-08-06 16:34:53 +0300

    Add change

--- Programming-Basic/Loops/Problem7-CalculateCombinations/Program.cs
+++ Programming-Basic/Loops/Problem7-CalculateCombinations/Program.cs
@@ -4,7 +4,7 @@ using System.Xml.Schema;
 public class CalculateCombination
 {
     /// <summary>
-    /// n! / (k! * (n-k)!) 
+    ///  A program that calculates n! / (k! * (n-k)!) for given n and k. 
     /// </summary>
     public static void Main()
     {
