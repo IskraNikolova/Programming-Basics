51e38c91ce313493842f37fc3b733da11540492f
Iskra Nikolova <air_iskra@abv.bg>
2015-08-08 13:35:36 +0300

    Add new problem

---
+++ Programming-Basic/Loops/Problem18-TrailingZeroesInN/TrailingZeroesInN.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+public class TrailingZeroesInN
+{
+    /// <summary>
+    /// Write a program that calculates with how many zeroes the factorial of a given number n has at its end. 
+    /// Your program should work well for very big numbers, e.g. n=100000. 
+    /// </summary>
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        BigInteger factorial = n;
+        for (int i = 1; i < n; i++)
+        {
+            factorial *= n - i;
+        }
+
+        string factorialToString = factorial.ToString();
+        int countOfZero = 0;
+        for (int i = factorialToString.Length - 1; i > 0 ; i--)
+        {
+            if (factorialToString[i] != '0')
+            {
+                break;
+            }
+            else
+            {
+                countOfZero++;
+            }
+        }
+
+        Console.WriteLine(countOfZero);
+    }
+}
+
