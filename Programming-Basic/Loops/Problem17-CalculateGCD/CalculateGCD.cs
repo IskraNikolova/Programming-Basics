2260345691b05805242d214b3c9b06813fb5491f
Iskra Nikolova <air_iskra@abv.bg>
2015-08-08 12:51:43 +0300

    Add new problem

---
+++ Programming-Basic/Loops/Problem17-CalculateGCD/CalculateGCD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class CalculateGCD
+{
+    /// <summary>
+    /// Write a program that calculates the greatest common divisor (GCD) of given
+    ///  two integers a and b. Use the Euclidean algorithm (find it in Internet). 
+    /// </summary>
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int c = a%b;
+        while (a%b != 0)
+        {
+            a = b;
+            b = c;
+            c = a%b;
+        }
+        Console.WriteLine(b);
+    }
+}
