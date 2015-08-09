cd261524d3de7f918b6df8454991e3eaf8b52a15
Iskra Nikolova <air_iskra@abv.bg>
2015-08-09 16:34:34 +0300

    Add new problem

---
+++ Programming-Basic/AdvancedTopics/Problem2-SortingNumbers/SortingNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class SortingNumbers
+{
+    /// <summary>
+    /// Write a program that reads a number n and a sequence of n integers, sorts them and prints them.
+    /// </summary>
+    public static void Main()
+    {
+        Console.Write("Write n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Write n numbers.");
+        int[] numbersToSort = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+            numbersToSort[i] = number;
+        }
+
+        Array.Sort(numbersToSort);
+
+        foreach (var number in numbersToSort)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
+
