d8de5c403814272cf81542e0341d586888217422
Iskra Nikolova <air_iskra@abv.bg>
2015-08-09 16:22:36 +0300

    Add new homework

---
+++ Programming-Basic/AdvancedTopics/Problem1-DifferenceBetweenDates/DifferenceBetweenDates.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class DifferenceBetweenDates
+{
+    /// <summary>
+    /// A program that enters two dates in format dd.MM.yyyy and returns the number of days between them. 
+    /// </summary>
+    public static void Main()
+    {
+        Console.Write("Write first date in format (dd.MM.yyyy): ");
+        DateTime firstDate = DateTime.Parse(Console.ReadLine());
+
+        Console.Write("Write second date in format (dd.MM.yyyy): ");
+        DateTime secondDate = DateTime.Parse(Console.ReadLine());
+
+        
+        Console.WriteLine("Days between: {0}", (secondDate - firstDate).Days);
+    }
+}
+
