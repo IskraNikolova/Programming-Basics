7fedd8f8b79b5f648de81488bb678e9aca3f8752
Iskra Nikolova <air_iskra@abv.bg>
2015-08-12 01:23:02 +0300

    Add e new problem

---
+++ Programming-Basic/AdvancedTopics/Problem6-RemoveNames/RemoveNames.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class RemoveNames
+{
+    /// <summary>
+    /// A program that takes as input two lists of names and removes from the first
+    ///  list all names given in the second list. The input and output lists are given as words,
+    ///  separated by a space, each list at a separate line.
+    /// </summary>
+    public static void Main()
+    {
+        string[] firstListOfNames = Console.ReadLine().Split(' ');
+
+        string[] secondListOfNames = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < firstListOfNames.Length; i++)
+        {
+            for (int j = 0; j < secondListOfNames.Length; j++)
+            {
+                if (firstListOfNames[i] == secondListOfNames[j])
+                {
+                    firstListOfNames[i] = null;
+                }
+            }
+        }
+
+        for (int i = 0; i < firstListOfNames.Length; i++)
+        {
+            if (!string.IsNullOrEmpty(firstListOfNames[i]))
+            {
+                Console.Write("{0} ",firstListOfNames[i]);
+            }
+            
+        }
+        Console.WriteLine();
+    }
+}
+
