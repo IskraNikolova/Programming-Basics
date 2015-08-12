4a2c0bd927616d88716feac5e7b681cdc5bcbdd7
Iskra Nikolova <air_iskra@abv.bg>
2015-08-12 20:00:53 +0300

    Add a new Problem

---
+++ Programming-Basic/AdvancedTopics/Problem9-ExtractURLsFromText/ExtractURLsFromText.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class ExtractURLsFromText
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        string pattern = @"(www\.\w+\.\w+)|(http:\/\/\w+\.?\w+\.?\w+)";
+
+        Regex regex = new Regex(pattern);
+
+        MatchCollection matches = regex.Matches(text);
+
+        foreach (var match in matches)
+        {
+            Console.WriteLine(match);
+        }
+       
+
+    }
+}
+
