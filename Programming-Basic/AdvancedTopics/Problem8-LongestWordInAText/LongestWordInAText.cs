d758e2756f0472b379b8ca9678e44d177693522a
Iskra Nikolova <air_iskra@abv.bg>
2015-08-12 03:22:51 +0300

    Add a new Problem

---
+++ Programming-Basic/AdvancedTopics/Problem8-LongestWordInAText/LongestWordInAText.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class LongestWordInAText
+{
+    /// <summary>
+    /// Write a program to find the longest word in a text.
+    /// </summary>
+    public static void Main()
+    {
+        string[] text = Console.ReadLine().Split(' ');
+
+        string longestWord = string.Empty;
+        int count = 0;
+        int longestCount = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            count = text[i].Length;
+            if (count > longestCount)
+            {
+                longestCount = count;
+                longestWord = text[i];
+            }
+        }
+
+        Console.WriteLine(longestWord);
+    }
+}
+
