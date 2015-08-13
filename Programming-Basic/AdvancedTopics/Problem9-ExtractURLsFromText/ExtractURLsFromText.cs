b06e1361096c048f9e3e0ecb1e95853aafd913c8
Iskra Nikolova <air_iskra@abv.bg>
2015-08-14 01:19:23 +0300

    Add a new Problem

--- Programming-Basic/AdvancedTopics/Problem9-ExtractURLsFromText/ExtractURLsFromText.cs
+++ Programming-Basic/AdvancedTopics/Problem9-ExtractURLsFromText/ExtractURLsFromText.cs
@@ -3,6 +3,11 @@ using System.Text.RegularExpressions;
 
 public class ExtractURLsFromText
 {
+    /// <summary>
+    /// A program that extracts and prints all URLs from given text. URL can be in only two formats:
+    ///http://something, e.g. http://softuni.bg, http://forums.softuni.bg, http://www.nakov.com 
+    ///www.something.domain, e.g.www.nakov.com, www.softuni.bg, www.google.com
+    /// </summary>
     public static void Main()
     {
         string text = Console.ReadLine();
@@ -16,8 +21,6 @@ public class ExtractURLsFromText
         {
             Console.WriteLine(match);
         }
-       
-
-    }
+     }
 }
 
---
+++ Programming-Basic/AdvancedTopics/Problem9-InsideTheBuilding/InsideTheBuilding.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class InsideTheBuilding
+{
+    public static void Main()
+    {
+        const int countOfPoint = 5;
+        
+        int h = int.Parse(Console.ReadLine());
+        for (int i = 0; i < countOfPoint; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if ((x >= 0 && x <= 3 * h) && (y >= 0 && y <= h) || (x <= 2 * h && x >= h) && (y >= h && y <= 4 * h))
+            {
+                Console.WriteLine("inside");
+            }
+            else
+            {
+                Console.WriteLine("outside");
+            }
+        }
+    }
+}
+
